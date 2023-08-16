25262e58e5ef99137f63736843bd445078ef99fe
Aki Wu <wuminzhe@gmail.com>
2023-08-16 14:49:16 +0800

    add estimate fees api

--- src/index.ts
+++ src/index.ts
@@ -97,6 +97,46 @@ app.get('/:platform/estimate_fee', (req: Request, res: Response) => {
   Effect.runPromise(estimateFee).catch((e) => errorWith(res, 1, e.message));
 });
 
+app.get('/estimate_fees', (req: Request, res: Response) => {
+  ////////////////////
+  // Request Params
+  ////////////////////
+  const fromChainId = req.query.from_chain_id as string
+  const toChainId = req.query.to_chain_id as string
+  const gasLimit = req.query.gas_limit as string
+  const payload: string = req.query.payload as string
+  const fromAddress: string = req.query.from_address as string
+  const toAddress: string = req.query.to_address as string
+  const extra: string = req.query.extra as string; // extra=[[1, 10]]
+  console.log(`fromChain: ${fromChainId}, toChain: ${toChainId}, gasLimit: ${gasLimit})`)
+  console.log(`payload: ${payload}, fromAddress: ${fromAddress}, toAddress: ${toAddress})`)
+  if (!fromChainId || !toChainId || !gasLimit || !payload || !fromAddress || !toAddress || !extra) {
+    errorWith(res, 1, `'from_chain_id', 'to_chain_id', 'gas_limit', 'payload', 'from_address', 'to_address' and 'extra' is required`)
+    return;
+  }
+
+  const estimateFees = pipe(
+    effectCheckParams(fromChainId, toChainId, gasLimit, fromAddress, toAddress, extra),
+    Effect.flatMap(params =>
+
+      Effect.all([
+        lzEstimateFee(params.fromChainIdInt, params.toChainIdInt, params.gasLimitInt, payload, fromAddress, toAddress, params.extraParams),
+        axEstimateFee(params.fromChainIdInt, params.toChainIdInt, params.gasLimitInt, payload, fromAddress, toAddress, params.extraParams),
+        celerEstimateFee(params.fromChainIdInt, params.toChainIdInt, params.gasLimitInt, payload, fromAddress, toAddress, params.extraParams)
+      ], { concurrency: 'inherit' })
+
+    ),
+    Effect.map((result) => ok(res, {
+      layerzero: result[0],
+      axelar: result[1],
+      celer: result[2]
+    }))
+  )
+
+  Effect.runPromise(estimateFees).catch((e) => errorWith(res, 1, e.message));
+
+})
+
 
 function effectCheckParams(fromChainId: string, toChainId: string, gasLimit: string, fromAddress: string, toAddress: string, extra: any) {
   return Effect.try({
