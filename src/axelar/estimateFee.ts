d15e5ad3b1fa60dcf3d489ed61f5366350730987
Aki Wu <wuminzhe@gmail.com>
2023-08-17 13:03:26 +0800

    merge axelar and axelar testnet

--- src/axelar/estimateFee.ts
+++ src/axelar/estimateFee.ts
@@ -8,17 +8,17 @@ import chainInfoTestnet from "./chainInfoTestnet";
 import { AxelarError } from "../errors";
 import { Effect as E } from "effect";
 
-const buildEstimateFee = (environment: Environment): IEstimateFee => {
-  const sdk = new AxelarQueryAPI({
-    environment: environment,
-  });
+type ChainInfo = [Environment, string, string];
 
-  const effectEstimateGasFee = (fromChainStr: string, toChainStr: string, srcGasToken: string, gasLimit: number) => {
+const buildEstimateFee = (): IEstimateFee => {
+
+  const effectEstimateGasFee = (fromChainInfo: ChainInfo, toChainInfo: ChainInfo, gasLimit: number) => {
+    const sdk = getAxelarSdk(fromChainInfo[0])
     return E.tryPromise({
       try: () => sdk.estimateGasFee(
-        fromChainStr,
-        toChainStr,
-        srcGasToken,
+        fromChainInfo[1],
+        toChainInfo[1],
+        fromChainInfo[2],
         gasLimit,
       ),
       catch: (error) => new AxelarError(`${error}`)
@@ -31,9 +31,10 @@ const buildEstimateFee = (environment: Environment): IEstimateFee => {
     gasLimit
   ) => {
     return E.Do.pipe(
-      E.bind("fromChainInfo", () => effectGetChainInfo(environment, fromChain)),
-      E.bind("toChainInfo", () => effectGetChainInfo(environment, toChain)),
-      E.flatMap(({ fromChainInfo, toChainInfo }) => effectEstimateGasFee(fromChainInfo[0], toChainInfo[0], fromChainInfo[1], gasLimit)),
+      E.bind("fromChainInfo", () => effectGetChainInfo(fromChain)),
+      E.bind("toChainInfo", () => effectGetChainInfo(toChain)),
+      E.flatMap(({ fromChainInfo, toChainInfo }) => checkSameEnvironment(fromChainInfo, toChainInfo)),
+      E.flatMap(({ fromChainInfo, toChainInfo }) => effectEstimateGasFee(fromChainInfo, toChainInfo, gasLimit)),
       E.map((result) => result as string)
     )
   }
@@ -41,24 +42,39 @@ const buildEstimateFee = (environment: Environment): IEstimateFee => {
   return estimateFee;
 }
 
-function effectGetChainInfo(environment: Environment, chainId: number) {
+function checkSameEnvironment(fromChainInfo: [Environment, string, string], toChainInfo: [Environment, string, string]) {
+  if (fromChainInfo[0] != toChainInfo[0]) {
+    return E.fail(new AxelarError(`fromChain ${fromChainInfo[0]} and toChain ${toChainInfo[0]} are not in the same environment`))
+  }
+
+  return E.succeed({ fromChainInfo, toChainInfo })
+}
+
+function getAxelarSdk(environment: Environment) {
+  return new AxelarQueryAPI({
+    environment: environment,
+  });
+}
+
+function effectGetChainInfo(chainId: number) {
   return E.try({
-    try: () => getChainInfo(environment, chainId),
+    try: () => getChainInfo(chainId),
     catch: (e) => new AxelarError(`${e}`)
   });
 }
 
-function getChainInfo(environment: Environment, chainId: number): [string, string] {
-  let chain;
-  if (environment === Environment.MAINNET) {
-    chain = chainInfo[chainId];
-  } else {
-    chain = chainInfoTestnet[chainId];
+function getChainInfo(chainId: number): [Environment, string, string] {
+  let chain = chainInfo[chainId];
+  if (chain) {
+    return [Environment.MAINNET, chain[0], chain[1]];
   }
-  if (!chain) {
-    throw `chain id ${chainId} not found`
+
+  chain = chainInfoTestnet[chainId];
+  if (chain) {
+    return [Environment.TESTNET, chain[0], chain[1]];
   }
-  return chain;
+
+  throw `chain id ${chainId} not found`
 }
 
 export default buildEstimateFee;
